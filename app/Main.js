fc3fc33390e07609974ea28c558c774e55d7b5e4
robertohastings <rob.hst@gmail.com>
2024-05-29 17:51:14 -0600

    39. View Single Post Screen

--- app/Main.js
+++ app/Main.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Axios from "axios"
+
+Axios.defaults.baseURL = "http://localhost:8080"
 
 //My Components
 import Header from "./components/Header"
@@ -9,6 +12,8 @@ import Home from "./components/Home"
 import Footer from "./components/Footer"
 import About from "./components/About"
 import Terms from "./components/Terms"
+import CreatePost from "./components/CreatePost"
+import ViewSinglePost from "./components/ViewSinglePost"
 
 function Main() {
     const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem("complexappToken")))
@@ -17,6 +22,8 @@ function Main() {
             <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
             <Routes>
                 <Route path="/" element={loggedIn ? <Home /> : <HomeGuest />} />
+                <Route path="/post/:id" element={<ViewSinglePost />} />
+                <Route path="/create-post" element={<CreatePost />} />
                 <Route path="/about-us" element={<About />} />
                 <Route path="/terms" element={<Terms />} />
             </Routes>
