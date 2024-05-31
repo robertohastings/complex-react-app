bcae0b04a35d6a871d0c6c274ab3abf5321134c3
robertohastings <35307304+robertohastings@users.noreply.github.com>
2024-05-31 06:27:12 -0600

    46. Profile Screen

--- app/Main.js
+++ app/Main.js
@@ -19,6 +19,7 @@ import Terms from "./components/Terms"
 import CreatePost from "./components/CreatePost"
 import ViewSinglePost from "./components/ViewSinglePost"
 import FlashMessages from "./components/FlashMessages"
+import Profile from './components/Profile'
 //import { stat } from "@babel/core/lib/gensync-utils/fs"
 //import { validate } from "webpack"
 //import { stat } from "@babel/core/lib/gensync-utils/fs"
@@ -68,6 +69,7 @@ function Main() {
                     <FlashMessages messages={state.flashMessages} />
                     <Header />
                     <Routes>
+                        <Route path="/profile/:username/*" element={<Profile />} />
                         <Route path="/" element={state.loggedIn ? <Home /> : <HomeGuest />} />
                         <Route path="/post/:id" element={<ViewSinglePost />} />
                         <Route path="/create-post" element={<CreatePost />} />
