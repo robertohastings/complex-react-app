5432e5e73def1dd7ed779d39716b8015f79efc26
robertohastings <rob.hst@gmail.com>
2024-06-03 18:27:33 -0600

    62. Finishing Search (Part 1)

--- app/components/Search.js
+++ app/components/Search.js
@@ -1,89 +1,126 @@
 import React, { useContext, useEffect } from "react"
+import { Link, useNavigate } from "react-router-dom"
 import DispatchContext from "../DispatchContext"
 import { useImmer } from "use-immer"
+import Axios from "axios"
 //import { stat } from "@babel/core/lib/gensync-utils/fs"
 
 function Search() {
+    const navigate = useNavigate()
+
     const appDispatch = useContext(DispatchContext)
 
     const [state, setState] = useImmer({
-      searchTerm: '',
-      results: [],
-      show: 'neither',
-      requestCount: 0
+        searchTerm: "",
+        results: [],
+        show: "neither",
+        requestCount: 0
     })
 
     useEffect(() => {
-        document.addEventListener('keyup', searchKeyPressHandler)
-        return () => document.removeEventListener('keyup', searchKeyPressHandler)
+        document.addEventListener("keyup", searchKeyPressHandler)
+        return () => document.removeEventListener("keyup", searchKeyPressHandler)
     }, [])
 
     useEffect(() => {
-      const delay = setTimeout(() => {
-        setState(draft => {
-          draft.requestCount++
-        } )
-      }, 3000)
-      return () => clearTimeout(delay)
+        if (state.searchTerm.trim()) {
+            setState(draft => {
+                draft.show = "loading"
+            })
+            const delay = setTimeout(() => {
+                setState(draft => {
+                    draft.requestCount++
+                })
+            }, 750)
+            return () => clearTimeout(delay)
+        } else {
+            setState(draft => {
+                draft.show = "neither"
+            })
+        }
     }, [state.searchTerm])
 
     useEffect(() => {
-      if (state.requestCount) {
-        //send actio  request here
-      }
+        if (state.requestCount) {
+            const ourRequest = Axios.CancelToken.source()
+            async function fetchResults() {
+                try {
+                    const response = await Axios.post("/search", { searchTerm: state.searchTerm }, { cancelToken: ourRequest.token })
+                    console.log(response.data)
+                    setState(draft => {
+                        draft.results = response.data
+                        draft.show = "results"
+                    })
+                } catch (e) {
+                    console.log("There was a problem or the request was cancelled")
+                }
+            }
+            fetchResults()
+            return () => ourRequest.cancel()
+        }
     }, [state.requestCount])
 
     function searchKeyPressHandler(e) {
         if (e.keyCode == 27) {
-            appDispatch({type: 'closeSearch'})
+            appDispatch({ type: "closeSearch" })
         }
     }
 
     function handleInput(e) {
-      const value = e.target.value
-      setState(draft => {
-        draft.searchTerm = value
-      })
+        const value = e.target.value
+        setState(draft => {
+            draft.searchTerm = value
+        })
     }
 
-  return (
-    <div className="search-overlay">
-      <div className="search-overlay-top shadow-sm">
-        <div className="container container--narrow">
-          <label htmlFor="live-search-field" className="search-overlay-icon">
-            <i className="fas fa-search"></i>
-          </label>
-          <input onChange={handleInput} autoFocus type="text" autoComplete="off" id="live-search-field" className="live-search-field" placeholder="What are you interested in?" />
-          <span onClick={() => appDispatch({type: 'closeSearch'})} className="close-live-search">
-            <i className="fas fa-times-circle"></i>
-          </span>
-        </div>
-      </div>
+    function handlerOnClick(e) {
+        e.preventDefault()
+        // navigate({`/post/${post._id}`})
+        console.log("hhandlerOnClickere", e.target.value)
+    }
 
-      <div className="search-overlay-bottom">
-        <div className="container container--narrow py-3">
-          <div className="live-search-results live-search-results--visible">
-            <div className="list-group shadow-sm">
-              <div className="list-group-item active"><strong>Search Results</strong> (3 items found)</div>
-              <a href="#" className="list-group-item list-group-item-action">
-                <img className="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" /> <strong>Example Post #1</strong>
-                <span className="text-muted small">by brad on 2/10/2020 </span>
-              </a>
-              <a href="#" className="list-group-item list-group-item-action">
-                <img className="avatar-tiny" src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128" /> <strong>Example Post #2</strong>
-                <span className="text-muted small">by barksalot on 2/10/2020 </span>
-              </a>
-              <a href="#" className="list-group-item list-group-item-action">
-                <img className="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" /> <strong>Example Post #3</strong>
-                <span className="text-muted small">by brad on 2/10/2020 </span>
-              </a>
+    return (
+        <div className="search-overlay">
+            <div className="search-overlay-top shadow-sm">
+                <div className="container container--narrow">
+                    <label htmlFor="live-search-field" className="search-overlay-icon">
+                        <i className="fas fa-search"></i>
+                    </label>
+                    <input onChange={handleInput} autoFocus type="text" autoComplete="off" id="live-search-field" className="live-search-field" placeholder="What are you interested in?" />
+                    <span onClick={() => appDispatch({ type: "closeSearch" })} className="close-live-search">
+                        <i className="fas fa-times-circle"></i>
+                    </span>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </div>
 
-  )
+            <div className="search-overlay-bottom">
+                <div className="container container--narrow py-3">
+                    <div className={"circle-loader " + (state.show == "loading" ? "circle-loader--visible" : "")}></div>
+                    <div className={"live-search-results " + (state.show == "results" ? "live-search-results--visible" : "")}>
+                        <div className="list-group shadow-sm">
+                            <div className="list-group-item active">
+                                <strong>Search Results</strong> (3 items found)
+                            </div>
+                            {state.results.map(post => {
+                                const date = new Date(post.createdDate)
+                                const dateFormatted = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
+
+                                return (
+                                    <a onClick={handlerOnClick} id={post._id} to={`/post/${post._id}`} className="list-group-item list-group-item-action">
+                                        <img className="avatar-tiny" src={post.author.avatar} /> <strong>{post.title}</strong>
+                                        <span className="text-muted small">
+                                            {" "}
+                                            {post.author.username} on {dateFormatted}{" "}
+                                        </span>
+                                    </a>
+                                )
+                            })}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Search
